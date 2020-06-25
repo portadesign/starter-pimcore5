eacb3318dd566fb1cff1471f2bc7a42c6fea9666
Andrea Navrátilová <andy.navratilova@gmail.com>
2020-06-25 17:32:28 +0200

    basePage option for staticroutes

--- bundles/AdminBundle/Controller/Admin/SettingsController.php
+++ bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -891,9 +891,23 @@ class SettingsController extends AdminController
             } elseif ($request->get('xaction') == 'update') {
                 // save routes
                 $route = Staticroute::getById($data['id']);
+
+                if ($data["basePage"]) {
+                    if ($doc = Document::getByPath($data["basePage"])) {
+                        $data["basePage"] = $doc->getId();
+                    }
+                }
+
                 $route->setValues($data);
 
                 $route->save();
+
+                $basePage = $route->getBasePage();
+                if (is_numeric($basePage)) {
+                    if ($doc = Document::getById(intval($basePage))) {
+                        $route->setBasePage($doc->getRealFullPath());
+                    }
+                }
 
                 return $this->adminJson(['data' => $route, 'success' => true]);
             } elseif ($request->get('xaction') == 'create') {
@@ -901,9 +915,23 @@ class SettingsController extends AdminController
 
                 // save route
                 $route = new Staticroute();
+
+                if ($data["basePage"]) {
+                    if ($doc = Document::getByPath($data["basePage"])) {
+                        $data["basePage"] = $doc->getId();
+                    }
+                }
+
                 $route->setValues($data);
 
                 $route->save();
+
+                $basePage = $route->getBasePage();
+                if (is_numeric($basePage)) {
+                    if ($doc = Document::getById(intval($basePage))) {
+                        $route->setBasePage($doc->getRealFullPath());
+                    }
+                }
 
                 return $this->adminJson(['data' => $route, 'success' => true]);
             }
@@ -933,6 +961,14 @@ class SettingsController extends AdminController
             $routes = [];
             /** @var Staticroute $route */
             foreach ($list->getRoutes() as $route) {
+                if ($basePage = $route->getBasePage()) {
+                    if (is_numeric($basePage)) {
+                        if ($doc = Document::getById(intval($basePage))) {
+                            $route->setBasePage($doc->getRealFullPath());
+                        }
+                    }
+                }
+
                 if (is_array($route->getSiteId())) {
                     $route = json_encode($route);
                     $route = json_decode($route, true);
